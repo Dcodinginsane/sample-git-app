838f2068d9731997da1baa74782b0be2fa0f74b1
Dcodinginsane <dlodhi2001@gmail.com>
2023-09-23 12:25:52 +0530

    Change the decription

--- aap.py
+++ aap.py
@@ -6,13 +6,12 @@ with col1:
     st.image('unnamed.png')
 
 with col2:
-    st.write("""
-A quantitative engineer, often referred to as a "quant engineer" or "quantitative engineer," is a professional who combines the skills of a quantitative analyst (quant) and an engineer. This role is typically found in the finance industry, where quantitative modeling and engineering techniques are used to develop and implement complex financial models and algorithms.
-""")
+    st.write('quantitative engineer')
 
 st.header('Quant skilll')
 st.subheader('Data scince')
 st.subheader('Mathematics and statics')
-st.subheader('Finance Anlysis')
-st.subheader('Mathematic applied in finace')
+
+st.subheader('Mathematic and statics applied in finace')
 st.subheader('SQL , databases')
+st.subheader('Resching')
