12d2aea977c728e7663bb3207aadb937bc837645
Dcodinginsane <dlodhi2001@gmail.com>
2023-09-23 10:34:21 +0530

    intial commit

---
+++ aap.py
@@ -0,0 +1,11 @@
+import  streamlit as st
+st.title("Deepak Lodhi")
+col1 , col2 = st.columns(2)
+
+with col1:
+    st.image('unnamed.png')
+
+with col2:
+    st.write("""
+A quantitative engineer, often referred to as a "quant engineer" or "quantitative engineer," is a professional who combines the skills of a quantitative analyst (quant) and an engineer. This role is typically found in the finance industry, where quantitative modeling and engineering techniques are used to develop and implement complex financial models and algorithms.
+""")
