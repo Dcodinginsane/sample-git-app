3d3726b9b41dc1595e1a6760f0cbbe58bcbcace4
Dcodinginsane <dlodhi2001@gmail.com>
2023-09-23 10:50:11 +0530

    added quant skill

--- aap.py
+++ aap.py
@@ -9,3 +9,8 @@ with col2:
     st.write("""
 A quantitative engineer, often referred to as a "quant engineer" or "quantitative engineer," is a professional who combines the skills of a quantitative analyst (quant) and an engineer. This role is typically found in the finance industry, where quantitative modeling and engineering techniques are used to develop and implement complex financial models and algorithms.
 """)
+
+st.header('Quant skilll')
+st.subheader('Data scince')
+st.subheader('Mathematics and statics')
+st.subheader('Finance Anlysis')
